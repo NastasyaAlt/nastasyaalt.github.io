892f80caf826d342f05caae4ca0aef840c6466e1
Anastasiia Smychkovska <nastasyaalt@gmail.com>
2020-05-01 22:11:29 +0300

    Portfolio1.0
    Portfolio 1.0

---
+++ build/js/main.js
@@ -0,0 +1,75 @@
+'use strict';
+window.onload = function() {
+
+	const easeInCubic = function (t) { return t*t*t }	
+	const scrollElems = document.getElementsByClassName('scroll_elem');
+	const scrollToElem = (start, stamp, duration, scrollEndElemTop, startScrollOffset) => {
+	//debugger;
+	const runtime = stamp - start;
+	let progress = runtime / duration;
+	const ease = easeInCubic(progress);
+
+	progress = Math.min(progress, 1);
+	console.log(startScrollOffset,startScrollOffset + (scrollEndElemTop * ease));
+
+	const newScrollOffset = startScrollOffset + (scrollEndElemTop * ease);
+	window.scroll(0, startScrollOffset + (scrollEndElemTop * ease));
+
+	if(runtime < duration){
+		requestAnimationFrame((timestamp) => {
+			const stamp = new Date().getTime();
+			scrollToElem(start, stamp, duration, scrollEndElemTop, startScrollOffset);
+			})
+		}
+	}
+
+	for(let i=0; i<scrollElems.length; i++){
+		const elem = scrollElems[i];
+
+		elem.addEventListener('click',function(e) {
+			e.preventDefault();
+			const scrollElemId = e.target.href.split('#')[1];
+			const scrollEndElem = document.getElementById(scrollElemId);
+
+			const anim = requestAnimationFrame(() => {
+				const stamp = new Date().getTime();
+				const duration = 500;
+				const start = stamp;
+
+				const startScrollOffset = window.pageYOffset;
+
+				const scrollEndElemTop = scrollEndElem.getBoundingClientRect().top;
+
+				scrollToElem(start, stamp, duration, scrollEndElemTop, startScrollOffset);
+				// scrollToElem(scrollEndElemTop);
+			})
+		})
+	}
+}
+
+function addClass(el, className) {
+    var el = document.querySelector(el);
+      if (el.classList) {
+        el.classList.add(className);
+      } else {
+        el.className += ' ' + className;
+      }
+
+ 	/*
+    for (let i = 0; i < el.length; i++) {
+      if (el.classList) {
+        el[i].classList.add(className);
+      } else {
+        el[i].className += ' ' + className;
+      }
+    }*/
+}
+
+function removeClass(el, className) {
+    var el = document.querySelector(el);
+      if (el.classList) {
+        el.classList.remove(className);
+      } else {
+        el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+      }
+}
